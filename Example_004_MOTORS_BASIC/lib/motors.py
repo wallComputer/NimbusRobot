e48664614ec13f230ef1f1edc6b7d4c82fd1272f
wallComputer <36563674+wallComputer@users.noreply.github.com>
2022-07-14 12:15:13 +0200

    Updated for direct control of all 4 motors at once. Added direct Switch control.

--- Example_004_MOTORS_BASIC/lib/motors.py
+++ Example_004_MOTORS_BASIC/lib/motors.py
@@ -1,4 +1,4 @@
-from machine import Pin,PWM
+from machine import Pin, PWM
 from nrpd import *
 from motorProfiles import *
 
@@ -12,38 +12,48 @@ def mapRange(oValue, oMin, oMax, nMin, nMax):
 
 class motorControllers:
     def __init__(self):
-        self.MSP = Pin(MOTOR_SWITCH_PIN, Pin.OUT)
+        self._MSP = Pin(MOTOR_SWITCH_PIN, Pin.OUT)
 
         self._MLT = 0
-        self.MLTSpeed = PWM(Pin(MLTIN1))
-        self.MLTSpeed.freq(MLTf)
-        self.MLTSpeed.duty_u16(65535)
-        self.MLTDirection = Pin(MLTIN2, Pin.OUT)
-        self.MLTDirection.value(1)
+        self.__MLTSpeed = PWM(Pin(MLTIN1))
+        self.__MLTSpeed.freq(MLTf)
+        self.__MLTSpeed.duty_u16(65535)
+        self.__MLTDirection = Pin(MLTIN2, Pin.OUT)
+        self.__MLTDirection.value(1)
         self._MLB = 0
-        self.MLBSpeed = PWM(Pin(MLBIN1))
-        self.MLBSpeed.freq(MLBf)
-        self.MLBSpeed.duty_u16(65535)
-        self.MLBDirection = Pin(MLBIN2, Pin.OUT)
-        self.MLBDirection.value(1)
+        self.__MLBSpeed = PWM(Pin(MLBIN1))
+        self.__MLBSpeed.freq(MLBf)
+        self.__MLBSpeed.duty_u16(65535)
+        self.__MLBDirection = Pin(MLBIN2, Pin.OUT)
+        self.__MLBDirection.value(1)
         self._MRT = 0
-        self.MRTSpeed = PWM(Pin(MRTIN1))
-        self.MRTSpeed.freq(MRTf)
-        self.MRTSpeed.duty_u16(65535)
-        self.MRTDirection = Pin(MRTIN2, Pin.OUT)
-        self.MRTDirection.value(1)
+        self.__MRTSpeed = PWM(Pin(MRTIN1))
+        self.__MRTSpeed.freq(MRTf)
+        self.__MRTSpeed.duty_u16(65535)
+        self.__MRTDirection = Pin(MRTIN2, Pin.OUT)
+        self.__MRTDirection.value(1)
         self._MRB = 0
-        self.MRBSpeed = PWM(Pin(MRBIN1))
-        self.MRBSpeed.freq(MRBf)
-        self.MRBSpeed.duty_u16(65535)
-        self.MRBDirection = Pin(MRBIN2, Pin.OUT)
-        self.MRBDirection.value(1)
+        self.__MRBSpeed = PWM(Pin(MRBIN1))
+        self.__MRBSpeed.freq(MRBf)
+        self.__MRBSpeed.duty_u16(65535)
+        self.__MRBDirection = Pin(MRBIN2, Pin.OUT)
+        self.__MRBDirection.value(1)
+
+        self._MA = [0, 0, 0, 0]  # Motor All Speed
 
     def turnMotionOff(self):
-        self.MSP.value(0)
+        self._MSP.value(0)
 
     def turnMotionOn(self):
-        self.MSP.value(1)
+        self._MSP.value(1)
+
+    @property
+    def MSP(self):
+        return self._MSP.value()
+
+    @MSP.setter
+    def MSP(self, value: int):
+        self._MSP.value(value)
 
     @property
     def MLT(self):
@@ -52,20 +62,21 @@ class motorControllers:
     @MLT.setter
     def MLT(self, speed):
         self._MLT = speed
+        self._MA[0] = speed
         dutyValue = 65535
         directionValue = 1
         if speed > 0:
             speed = mapRange(speed, 0, 100, MLTFm, 100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed/100*3.3))
-            dutyValue = int(speed*65535/100)
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed/100*3.3))
+            dutyValue = int(speed * 65535 / 100)
             directionValue = 0
         elif speed < 0:
             speed = mapRange(speed, 0, -100, MLTBm, -100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed/100*3.3))
-            dutyValue = int(65535 + speed*65535/100)
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed/100*3.3))
+            dutyValue = int(65535 + speed * 65535 / 100)
             directionValue = 1
-        self.MLTDirection.value(directionValue)
-        self.MLTSpeed.duty_u16(dutyValue)
+        self.__MLTDirection.value(directionValue)
+        self.__MLTSpeed.duty_u16(dutyValue)
 
     @property
     def MLB(self):
@@ -74,21 +85,21 @@ class motorControllers:
     @MLB.setter
     def MLB(self, speed):
         self._MLB = speed
+        self._MA[1] = speed
         dutyValue = 65535
         directionValue = 1
         if speed > 0:
             speed = mapRange(speed, 0, 100, MLBFm, 100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed / 100 * 3.3))
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed / 100 * 3.3))
             dutyValue = int(speed * 65535 / 100)
             directionValue = 0
         elif speed < 0:
             speed = mapRange(speed, 0, -100, MLBBm, -100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed / 100 * 3.3))
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed / 100 * 3.3))
             dutyValue = int(65535 + speed * 65535 / 100)
             directionValue = 1
-        self.MLBDirection.value(directionValue)
-        self.MLBSpeed.duty_u16(dutyValue)
-
+        self.__MLBDirection.value(directionValue)
+        self.__MLBSpeed.duty_u16(dutyValue)
 
     @property
     def MRT(self):
@@ -97,20 +108,21 @@ class motorControllers:
     @MRT.setter
     def MRT(self, speed):
         self._MRT = speed
+        self._MA[2] = speed
         dutyValue = 65535
         directionValue = 1
         if speed > 0:
             speed = mapRange(speed, 0, 100, MRTFm, 100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed/100*3.3))
-            dutyValue = int(speed*65535/100)
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed/100*3.3))
+            dutyValue = int(speed * 65535 / 100)
             directionValue = 0
         elif speed < 0:
             speed = mapRange(speed, 0, -100, MRTBm, -100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed/100*3.3))
-            dutyValue = int(65535 + speed*65535/100)
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed/100*3.3))
+            dutyValue = int(65535 + speed * 65535 / 100)
             directionValue = 1
-        self.MRTDirection.value(directionValue)
-        self.MRTSpeed.duty_u16(dutyValue)
+        self.__MRTDirection.value(directionValue)
+        self.__MRTSpeed.duty_u16(dutyValue)
 
     @property
     def MRB(self):
@@ -119,18 +131,29 @@ class motorControllers:
     @MRB.setter
     def MRB(self, speed):
         self._MRT = speed
+        self._MA[3] = speed
         dutyValue = 65535
         directionValue = 1
         if speed > 0:
             speed = mapRange(speed, 0, 100, MRBFm, 100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed / 100 * 3.3))
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=speed / 100 * 3.3))
             dutyValue = int(speed * 65535 / 100)
             directionValue = 0
         elif speed < 0:
             speed = mapRange(speed, 0, -100, MRBBm, -100)
-            print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed / 100 * 3.3))
+            # print("Set Speed% = {Speed:} VDIFF={VDIFF:0.2f}".format(Speed=speed, VDIFF=-speed / 100 * 3.3))
             dutyValue = int(65535 + speed * 65535 / 100)
             directionValue = 1
-        self.MRBDirection.value(directionValue)
-        self.MRBSpeed.duty_u16(dutyValue)
+        self.__MRBDirection.value(directionValue)
+        self.__MRBSpeed.duty_u16(dutyValue)
 
+    @property
+    def MA(self):
+        return self._MA
+
+    @MA.setter
+    def MA(self, speeds):
+        self.MLT = speeds[0]
+        self.MLB = speeds[1]
+        self.MRT = speeds[2]
+        self.MRB = speeds[3]
--- Example_004_MOTORS_BASIC/main.py
+++ Example_004_MOTORS_BASIC/main.py
@@ -5,6 +5,7 @@ from lib import motors
 print("Hello, World!")
 
 mc = motors.motorControllers()
+mc.MSP = 1
 
 for speed in range(0, 100, 2):
     mc.MLT = speed
@@ -15,10 +16,7 @@ for speed in range(0, 100, 2):
     time.sleep(0.1)
 
 for speed in range(100, -100, 2):
-    mc.MLT = speed
-    mc.MLB = speed
-    mc.MRT = speed
-    mc.MRB = speed
+    mc.MA = [speed, speed, speed, speed]
     print("Speed: {Speed:}".format(Speed=speed))
     time.sleep(0.2/4)
 
@@ -30,8 +28,5 @@ for speed in range(-100, 0, 2):
     print("Speed: {Speed:}".format(Speed=speed))
     time.sleep(0.1)
 
-
-mc.MLT = 0
-mc.MLB = 0
-mc.MRT = 0
-mc.MRB = 0
+mc.MSP = 0
+mc.MA = [0,0,0,0]
