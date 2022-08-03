b74b42c027b28c18596d5a278ed76abad1c2f71e
wallComputer <36563674+wallComputer@users.noreply.github.com>
2022-08-03 20:34:23 +0200

    added basic encoder and PID controller for motors.

--- Example_006_MOTORS_ENCODERS/controller.py
+++ Example_006_MOTORS_ENCODERS/controller.py
@@ -47,12 +47,17 @@ RBParams = {
     'PIDParams_Distance': [80, 0, 6],
     'PIDSamplingFrequency': 100
 }
+# time.sleep(3)
+t1 = time.ticks_ms()
 mc = motorHandler.nimbusMotorController(MOTOR_SWITCH_PIN,
                                         MLTParams=LTParams,
                                         MLBParams=LBParams,
                                         MRTParams=RTParams,
                                         MRBParams=RBParams)
+print(time.ticks_diff(time.ticks_ms(), t1))
 btn = Pin(BUTTON_PIN, Pin.IN)
+sp = (37*3.141592653589793)*5
+
 
 def btnIRQ(pin):
     global mc
@@ -61,10 +66,15 @@ def btnIRQ(pin):
 
 btn.irq(trigger=Pin.IRQ_FALLING, handler=btnIRQ)
 
-
-while True:
-    print("MLTEC:{0},MLBEC:{1},MRTEC:{2},MRBEC:{3}".format(mc.MLTEC(),
-                                                           mc.MLBEC(),
-                                                           mc.MRTEC(),
-                                                           mc.MRBEC()))
-    time.sleep(0.2)
+# duration = 16.0
+# mc.MSP = 1
+# startTime = time.ticks_ms()
+# while time.ticks_diff(time.ticks_ms(), startTime) / 1e3 < duration:
+#     mc.MLBDSP = sp
+#     while mc.MLBDSP != 0:
+#         print("SP:{0},E:{1}".format(mc.MLBDSP, mc.MLBD))
+#         mc.ELTC
+#         time.sleep(0.05)
+#     time.sleep(0.5)
+#     sp = -sp
+# mc.MSP = 0
